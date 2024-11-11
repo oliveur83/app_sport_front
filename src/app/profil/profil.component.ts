5a79534c611039801bc1271ab65f6699a3327013
oliveur83 <tomolivierpro@gmail.com>
2024-11-11 17:47:38 +0100

    tout debut de projet

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,HttpClientModule],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.css'
 })
@@ -18,4 +21,22 @@ goals = [
   { description: 'Participer à un semi-marathon', deadline: '2024-11-01' },
   { description: 'Améliorer le temps sur 5 km à moins de 25 minutes', deadline: '2024-10-01' }
 ];
+
+message: string = '';
+
+constructor(private http: HttpClient) { }
+
+// Fonction qui envoie les données au backend
+fetchHello() {
+  this.http.get('http://localhost:8080/', { responseType: 'text' })
+    .subscribe(
+      (response) => {
+        this.message = response; // Affiche la réponse "Hello, world!"
+        console.log('Réponse du serveur:', response);
+      },
+      (error) => {
+        console.error('Erreur:', error);
+      }
+    );
+}
 }
